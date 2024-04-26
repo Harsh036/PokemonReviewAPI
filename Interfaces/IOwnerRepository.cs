870ca5b36510d098fbfe9b095c09e1fd3dca4c2d
Harsh036 <64692744+Harsh036@users.noreply.github.com>
2024-04-27 00:40:50 +0530

    Add files via upload

---
+++ Interfaces/IOwnerRepository.cs
@@ -0,0 +1,22 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IOwnerRepository
+    {
+            ICollection<Owner> GetOwners();
+
+            Owner GetOwner(int ownerId);
+
+            ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+
+            ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+
+            bool OwnerExists(int OwnerId);
+
+
+        }
+    }
+
+
+
---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,40 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;                
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public Reviewer GetPokemonRating(int pokeId)
+        {
+            return _context.Reviewers.Where(p => p.Id == pokeId).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeid)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeid);     
+        }
+    }
+}
